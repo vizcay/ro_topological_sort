fb803cf77e8d5656ec5c899cff4428deb020c810
Pablo Vizcay <pabo.vizcay@gmail.com>
2015-04-02 18:05:37 -0300

    added topological sort usage example

---
+++ topological_sort_usage.rb
@@ -0,0 +1,13 @@
+require_relative 'topological_sort_1'
+
+grafo     = Grafo.new
+assembler = Nodo.new('Assembler')
+fortran   = Nodo.new('Fortran',   [assembler])
+c         = Nodo.new('C',         [assembler, fortran])
+lisp      = Nodo.new('Lisp',      [assembler])
+smalltalk = Nodo.new('Smalltalk', [fortran])
+perl      = Nodo.new('Perl',      [c])
+ruby      = Nodo.new('Ruby',      [smalltalk, lisp, perl])
+grafo.nodos << assembler << c << fortran << lisp << perl << ruby << smalltalk
+
+puts grafo.ordenamiento_topologico
